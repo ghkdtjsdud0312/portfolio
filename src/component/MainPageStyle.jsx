097c0a4b684301241cd88d8248bb9059c1113009
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-02-05 23:51:27 +0900

    메인 틀 작업중

---
+++ src/component/MainPageStyle.jsx
@@ -0,0 +1,8 @@
+import { styled } from "styled-components";
+
+const MainPageComp = styled.section`
+  border: 1px solid red;
+  text-align: center;
+`;
+
+export default MainPageComp;
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import MainPageComp from "../component/MainPageStyle";
+
+const MainPage = () => {
+  return (
+    <>
+      <MainPageComp>
+        <div className="container">
+          <div className="main">안녕하세요. 나는 [] 개발자 황선영 입니다.</div>
+        </div>
+      </MainPageComp>
+    </>
+  );
+};
+
+export default MainPage;
