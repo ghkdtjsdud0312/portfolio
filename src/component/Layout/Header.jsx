9969af297c2405c84a8f604487e5291504fed759
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-02-16 17:47:13 +0900

    깃허브,노션연결

--- src/component/Layout/Header.jsx
+++ src/component/Layout/Header.jsx
@@ -9,7 +9,8 @@ const HeaderComp = styled.header`
   z-index: 999; /* 다른 컨텐츠 위에 위치하도록 함 */
   .container {
     .menu {
-      display: flex; /* 메뉴 항목들을 가로로 배열 */
+      display: flex;
+      align-items: center;
       h2 {
         font-weight: 600;
         margin-right: auto;
@@ -26,6 +27,16 @@ const HeaderComp = styled.header`
           &:hover {
             color: var(--DARKGREY);
           }
+        }
+      }
+    }
+    .menu2 {
+      display: flex;
+      align-items: center;
+      li {
+        margin-left: 25%;
+        img {
+          cursor: pointer;
         }
       }
     }
@@ -60,6 +71,31 @@ const Header = () => {
           <li>
             <button onClick={() => scrollToSection("project")}>Project</button>
           </li>
+          <ul className="menu2">
+            <li>
+              <a
+                href="https://www.notion.so/KH-c6a74a27c5e14a40bd5ebf1cb81e0491"
+                target="_blank"
+                rel="noopener noreferrer">
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/kh-miniproject.appspot.com/o/%E1%84%82%E1%85%A9%E1%84%89%E1%85%A7%E1%86%AB2.svg?alt=media&token=18b8ae1c-bf15-49d3-873c-eeec51768b4e"
+                  alt="notion"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/ghkdtjsdud0312"
+                target="_blank"
+                rel="noopener noreferrer">
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/kh-miniproject.appspot.com/o/%E1%84%80%E1%85%B5%E1%86%BA%E1%84%92%E1%85%A5%E1%86%B8.webp?alt=media&token=fd5c4053-a890-408b-bc03-438fa2634d29"
+                  alt="github"
+                  style={{ width: "40%" }}
+                />
+              </a>
+            </li>
+          </ul>
         </ul>
       </div>
     </HeaderComp>
