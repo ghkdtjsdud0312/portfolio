c1d925d1b8506fb656a8f580c6755e66d167742d
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-03-08 12:33:57 +0900

    선영 코드 추가 및 수정중

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  BrowserRouter,
+} from "react-router-dom";
 import GlobalStyle from "./utils/GlobalStyle";
 import MainPage from "./pages/MainPage";
 import Layout from "./pages/Layout";
@@ -7,14 +12,17 @@ import "./App.css";
 function App() {
   return (
     <>
-      <GlobalStyle />
-      <Router>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<MainPage />} />
-          </Route>
-        </Routes>
-      </Router>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <GlobalStyle />
+        <Router>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<MainPage />} />
+            </Route>
+            <Route path="/search" element={<search />} />
+          </Routes>
+        </Router>
+      </BrowserRouter>
     </>
   );
 }
