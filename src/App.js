b22135b98d1b6c0cf77d0e4c3cccd00e013a8717
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-03-08 12:51:40 +0900

    index.js 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  BrowserRouter,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import GlobalStyle from "./utils/GlobalStyle";
 import MainPage from "./pages/MainPage";
 import Layout from "./pages/Layout";
@@ -13,12 +8,11 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router>
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<MainPage />} />
           </Route>
-          <Route path="/search" element={<search />} />
         </Routes>
       </Router>
     </>
